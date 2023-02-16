34d93e24e564aede8eedb7a1d781cd014cc3540e
Sprof22 <richmondelaigwu@gmail.com>
2023-02-16 22:31:31 +0100

    installed dependecies

---
+++ src/components/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Accordion } from 'react-accessible-accordion'
+
+const Search = () => {
+  return (
+    <Accordion></Accordion>
+  )
+}
+
+export default Search
