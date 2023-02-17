8ea49a649d1e0576518f2cb7865c944588f85816
Sprof22 <richmondelaigwu@gmail.com>
2023-02-17 22:28:19 +0100

    iron man instead

--- src/App.js
+++ src/App.js
@@ -9,85 +9,75 @@ import Search from "./components/Search";
 function App() {
   const [movies, setMovies] = useState([
     {
-      Title: "Star Wars: Episode IV - A New Hope",
-      Year: "1977",
-      imdbID: "tt0076759",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode V - The Empire Strikes Back",
-      Year: "1980",
-      imdbID: "tt0080684",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode VI - Return of the Jedi",
-      Year: "1983",
-      imdbID: "tt0086190",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode VII - The Force Awakens",
-      Year: "2015",
-      imdbID: "tt2488496",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode I - The Phantom Menace",
-      Year: "1999",
-      imdbID: "tt0120915",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode III - Revenge of the Sith",
-      Year: "2005",
-      imdbID: "tt0121766",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode II - Attack of the Clones",
-      Year: "2002",
-      imdbID: "tt0121765",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg",
-    },
-    {
-      Title: "Rogue One: A Star Wars Story",
-      Year: "2016",
-      imdbID: "tt3748528",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode VIII - The Last Jedi",
-      Year: "2017",
-      imdbID: "tt2527336",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode IX - The Rise of Skywalker",
-      Year: "2019",
-      imdbID: "tt2527338",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_SX300.jpg",
-    },
+            "Title": "Iron Man",
+            "Year": "2008",
+            "imdbID": "tt0371746",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Iron Man 3",
+            "Year": "2013",
+            "imdbID": "tt1300854",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjE5MzcyNjk1M15BMl5BanBnXkFtZTcwMjQ4MjcxOQ@@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Iron Man 2",
+            "Year": "2010",
+            "imdbID": "tt1228705",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_SX300.jpg"
+        },
+        {
+            "Title": "The Man in the Iron Mask",
+            "Year": "1998",
+            "imdbID": "tt0120744",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BZjM2YzcxMmQtOTc2Mi00YjdhLWFlZjUtNmFmMDQzYzU2YTk5L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+        },
+        {
+            "Title": "The Man with the Iron Fists",
+            "Year": "2012",
+            "imdbID": "tt1258972",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMTg5ODI3ODkzOV5BMl5BanBnXkFtZTcwMTQxNjUwOA@@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Tetsuo: The Iron Man",
+            "Year": "1989",
+            "imdbID": "tt0096251",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BODYxZTIwMWQtZTdiMS00ODRmLThlODEtNjkwNmE1ZTY1ZjM1XkEyXkFqcGdeQXVyNzc5MjA3OA@@._V1_SX300.jpg"
+        },
+        {
+            "Title": "The Man with the Iron Heart",
+            "Year": "2017",
+            "imdbID": "tt3296908",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BNWY2ZGMxYTQtOGY1Mi00N2ZhLThkYzgtNDhlZjhlNzg4MWU0XkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_SX300.jpg"
+        },
+        {
+            "Title": "The Invincible Iron Man",
+            "Year": "2007",
+            "imdbID": "tt0903135",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BOGRmZDg1YjMtMDA5YS00OTFjLTgyMjQtNDgzNTIyNzAwZDg0L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+        },
+        {
+            "Title": "The Man with the Iron Fists 2",
+            "Year": "2015",
+            "imdbID": "tt3625152",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BODkyMTMwMjA0Nl5BMl5BanBnXkFtZTgwMzQ3MDc4NDE@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Iron Man: Rise of Technovore",
+            "Year": "2013",
+            "imdbID": "tt2654124",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BNGJkNDQwNzUtNWE0MC00MGVjLWFjMjEtODMyNTExMTU4ZDRhL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+        }
   ]);
   const handleOnSearchChange = (searchData) => {
     const id = searchData.value;
