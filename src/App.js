db09459d7ea6b372165c1696b9b84cd594a412b4
Sprof22 <richmondelaigwu@gmail.com>
2023-02-17 23:39:14 +0100

    arranging

--- src/App.js
+++ src/App.js
@@ -7,41 +7,19 @@ import MovieList from "./components/MovieList";
 import Search from "./components/Search";
 
 const App = () => {
-  const [movies, setMovies] = useState([
-    
-    {
-      Title: "Iron Man 2",
-      Year: "2010",
-      imdbID: "tt1228705",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Man with the Iron Heart",
-      Year: "2017",
-      imdbID: "tt3296908",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNWY2ZGMxYTQtOGY1Mi00N2ZhLThkYzgtNDhlZjhlNzg4MWU0XkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_SX300.jpg",
-    },
-    {
-      Title: "Iron Man: Rise of Technovore",
-      Year: "2013",
-      imdbID: "tt2654124",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNGJkNDQwNzUtNWE0MC00MGVjLWFjMjEtODMyNTExMTU4ZDRhL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+  const [searchInput, setSearchInput] =useState('')
   const handleOnSearchChange = (searchData) => {
     console.log(searchData)
   }
 
-  const getMovieRequest = async (searchValue) => {
+  const getMovieRequest = async () => {
 
-		const response = await fetch(`${OMDB_API_URL}/?s=${searchValue}&apikey=${OMDB_API_KEY}`);
+		const response = await fetch(`${OMDB_API_URL}/?s=avengers&apikey=${OMDB_API_KEY}`);
 		const responseJson = await response.json();
+
+    setMovies(responseJson.Search)
+    console.log(responseJson)
   }
     useEffect(() => {
       getMovieRequest() 
@@ -49,7 +27,10 @@ const App = () => {
     
   return (
     <div className="container-fluid full-page">
-      <Search onSearchChange={handleOnSearchChange} />
+      <div className="row">
+        <div className="heading"></div>
+        <div className="search"><Search onSearchChange={handleOnSearchChange} /></div>
+      </div>
       <div className="d-flex">
         <MovieList movies={movies} />
       </div>
--- src/api.js
+++ src/api.js
@@ -8,5 +8,5 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://online-movie-database.p.rapidapi.com";
 
 
-export const OMDB_API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=6cb31e54"
-export const OMDB_API_KEY = "h6cb31e54"
+export const OMDB_API_URL = "http://www.omdbapi.com"
+export const OMDB_API_KEY = "6cb31e54"
