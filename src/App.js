bcca12e5c9199c87a0f15b9f8168aaba451b0dbb
Sprof22 <richmondelaigwu@gmail.com>
2023-02-17 23:17:07 +0100

    useEffect called

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { OMDB_API_KEY, OMDB_API_URL } from "./api";
@@ -6,24 +6,9 @@ import MovieList from "./components/MovieList";
 
 import Search from "./components/Search";
 
-function App() {
+const App = () => {
   const [movies, setMovies] = useState([
-    {
-      Title: "Iron Man",
-      Year: "2008",
-      imdbID: "tt0371746",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg",
-    },
-    {
-      Title: "Iron Man 3",
-      Year: "2013",
-      imdbID: "tt1300854",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjE5MzcyNjk1M15BMl5BanBnXkFtZTcwMjQ4MjcxOQ@@._V1_SX300.jpg",
-    },
+    
     {
       Title: "Iron Man 2",
       Year: "2010",
@@ -33,52 +18,12 @@ function App() {
         "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_SX300.jpg",
     },
     {
-      Title: "The Man in the Iron Mask",
-      Year: "1998",
-      imdbID: "tt0120744",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BZjM2YzcxMmQtOTc2Mi00YjdhLWFlZjUtNmFmMDQzYzU2YTk5L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Man with the Iron Fists",
-      Year: "2012",
-      imdbID: "tt1258972",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTg5ODI3ODkzOV5BMl5BanBnXkFtZTcwMTQxNjUwOA@@._V1_SX300.jpg",
-    },
-    {
-      Title: "Tetsuo: The Iron Man",
-      Year: "1989",
-      imdbID: "tt0096251",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BODYxZTIwMWQtZTdiMS00ODRmLThlODEtNjkwNmE1ZTY1ZjM1XkEyXkFqcGdeQXVyNzc5MjA3OA@@._V1_SX300.jpg",
-    },
-    {
       Title: "The Man with the Iron Heart",
       Year: "2017",
       imdbID: "tt3296908",
       Type: "movie",
       Poster:
         "https://m.media-amazon.com/images/M/MV5BNWY2ZGMxYTQtOGY1Mi00N2ZhLThkYzgtNDhlZjhlNzg4MWU0XkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_SX300.jpg",
-    },
-    {
-      Title: "The Invincible Iron Man",
-      Year: "2007",
-      imdbID: "tt0903135",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BOGRmZDg1YjMtMDA5YS00OTFjLTgyMjQtNDgzNTIyNzAwZDg0L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Man with the Iron Fists 2",
-      Year: "2015",
-      imdbID: "tt3625152",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BODkyMTMwMjA0Nl5BMl5BanBnXkFtZTgwMzQ3MDc4NDE@._V1_SX300.jpg",
     },
     {
       Title: "Iron Man: Rise of Technovore",
@@ -90,22 +35,25 @@ function App() {
     },
   ]);
   const handleOnSearchChange = (searchData) => {
-    const id = searchData.value;
-
-    const movieListFetch = fetch(
-      `${OMDB_API_URL}/?s=${id}&apikey=${OMDB_API_KEY}`
-    ).then(async(response) => {
-        const movieListResponse = await response.json()
-      })
+    console.log(searchData)
   }
+
+  const getMovieRequest = async (searchValue) => {
+
+		const response = await fetch(`${OMDB_API_URL}/?s=${searchValue}&apikey=${OMDB_API_KEY}`);
+		const responseJson = await response.json();
+  }
+    useEffect(() => {
+      getMovieRequest() 
+    }, [])
+    
   return (
     <div className="container-fluid full-page">
       <Search onSearchChange={handleOnSearchChange} />
       <div className="d-flex">
-        <MovieList movieListResponse={movieListResponse} />
+        <MovieList movies={movies} />
       </div>
     </div>
   );
 }
-
 export default App;
