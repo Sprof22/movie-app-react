4b2e1db00a4872cf1b9e3b702f62164e05594500
Sprof22 <richmondelaigwu@gmail.com>
2023-02-19 00:51:44 +0100

    Add Search Component

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { OMDB_API_KEY, OMDB_API_URL } from "./api";
 import MovieList from "./components/MovieList";
+import SearchAsync from "./components/SearchAsync";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchInput, setSearchInput] =useState('')
-  const handleOnSearchChange = (searchData) => {
-    console.log(searchData)
-  }
 
-  const getMovieRequest = async () => {
 
-		const response = await fetch(`${OMDB_API_URL}/?s=avengers&apikey=${OMDB_API_KEY}`);
+  const handleOnSearchChange = async (searchData) => {
+
+		const response = await fetch(`${OMDB_API_URL}/?s=spider man&apikey=${OMDB_API_KEY}`);
 		const responseJson = await response.json();
 
     setMovies(responseJson.Search)
     console.log(responseJson)
   }
     useEffect(() => {
-      getMovieRequest() 
+      handleOnSearchChange() 
     }, [])
     
   return (
