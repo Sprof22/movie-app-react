15f166c12f0c068826827ea8668858b845e77197
Sprof22 <richmondelaigwu@gmail.com>
2023-02-19 01:16:25 +0100

    new app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,142 @@
-import React, { useEffect, useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import { OMDB_API_KEY, OMDB_API_URL } from "./api";
-import MovieList from "./components/MovieList";
-import SearchAsync from "./components/SearchAsync";
+import { useState, useEffect } from "react";
+import MovieCard from "./components/MovieCard";
+import './styles/style.css'
 
-const App = () => {
-  const [movies, setMovies] = useState([]);
-  const [searchInput, setSearchInput] =useState('')
+// import './style.css'
+let API_keys = "&api_key=1189e19e4888756b7edb9a1346371528";
+let base_url = "https://api.themoviedb.org/3";
+let url = base_url + "/discover/movie?sort_by=popularity.desc" + API_keys;
+let arr = ["Popular", "Theater", "Kids", "Drama", "Comedy"];
 
+const Main = () => {
+  const [movieData, SetData] = useState([]);
+  const [url_set, setUrl] = useState(url);
+  const [search, setSearch] = useState("");
+  const [scrolled, setScrolled] = useState(false);
 
-  const handleOnSearchChange = async (searchData) => {
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
 
-		const response = await fetch(`${OMDB_API_URL}/?s=spider man&apikey=${OMDB_API_KEY}`);
-		const responseJson = await response.json();
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
 
-    setMovies(responseJson.Search)
-    console.log(responseJson)
+  let navbarClasses = ["navbar"];
+  if (scrolled) {
+    navbarClasses.push("scrolled");
   }
-    useEffect(() => {
-      handleOnSearchChange() 
-    }, [])
-    
+
+  useEffect(() => {
+    fetch(url_set)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data.results)
+        SetData(data.results);
+      });
+  }, [url_set]);
+
+  const getData = (movieType) => {
+    if (movieType === "Popular") {
+      url = base_url + "/discover/movie?sort_by=popularity.desc" + API_keys;
+    }
+    if (movieType === "Theater") {
+      url =
+        base_url +
+        "/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22" +
+        API_keys;
+    }
+    if (movieType === "Kids") {
+      url =
+        base_url +
+        "/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc" +
+        API_keys;
+    }
+    if (movieType === "Drama") {
+      url =
+        base_url +
+        "/discover/movie?with_genres=18&sort_by=vote_average.desc&vote_count.gte=10" +
+        API_keys;
+    }
+    if (movieType === "Comedy") {
+      url =
+        base_url +
+        "/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc" +
+        API_keys;
+    }
+    setUrl(url);
+  };
+
+  const searchMovie = (evt) => {
+    if (evt.key === "Enter") {
+      // console.log('hello')
+      url =
+        base_url +
+        "/search/movie?api_key=1189e19e4888756b7edb9a1346371528&query=" +
+        search;
+      setUrl(url);
+      setSearch(" ");
+    }
+  };
   return (
-    <div className="container-fluid full-page">
-      <div className="row d-flex justify-content-space between gap-3">
-      <div className="heading col-8 mt-2">Movie APP</div>
-      <div className="bg-danger col-4 m-0 mt-2"><SearchAsync onSearchChange={handleOnSearchChange}/></div>
-      
+    <>
+      <div className={navbarClasses.join(" ")}>
+        <div className="header">
+          <img src="images/movie-icon.png" alt="logo" className="w-15 h-12 cursor-pointer" />
+          <nav>
+            <ul>
+              {arr.map((value) => {
+                return (
+                  <li>
+                    <a
+                      href="#"
+                      name={value}
+                      onClick={(e) => {
+                        getData(e.target.name);
+                      }}
+                    >
+                      {value}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+          <form>
+            <div className="search-btn">
+              <input
+                type="text"
+                placeholder="Enter Movie Name"
+                className="inputText"
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+                value={search}
+                onKeyPress={searchMovie}
+              />
+              <button type="submit">
+                <i class="fas fa-search"></i>
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-      <div className="d-flex">
-        <MovieList movies={movies} />
+      <div className="container">
+        {movieData.length == 0 ? (
+          <p className="text-2xl font-bold text-red-900">Opps!!! Not found☹️</p>
+        ) : (
+          movieData.map((res, pos) => {
+            return <MovieCard info={res} key={pos} />;
+          })
+        )}
       </div>
-    </div>
+    </>
   );
-}
-export default App;
+};
+
+export default Main;
